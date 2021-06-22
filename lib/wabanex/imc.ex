b34ff2a3e99e48abf6eacfbe34e6f012fbf47ae6
allankildare <allankildare27@gmail.com>
2021-06-22 20:09:41 -0300

    feat: calculo de imc de um arquivo csv

---
+++ lib/wabanex/imc.ex
@@ -0,0 +1,28 @@
+defmodule Wabanex.IMC do
+  def calculate(filename) do
+    filename
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, content}) do
+    content
+    |> String.split("\n")
+    |> Enum.map(fn line -> parse_line(line) end)
+    |> Enum.into(%{})
+  end
+
+  defp handle_file({:error, _reason}) do
+    {:error, "Erro ao abrir o arquivo"}
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split(",")
+    |> List.update_at(1, &String.to_float/1)
+    |> List.update_at(2, &String.to_float/1)
+    |> calculate_imc()
+  end
+
+  defp calculate_imc([name, height, weight]), do: {name, weight / (height * height)}
+end
